a1bab432000aabc77b3ad0ae257c7e2ad7e12e4b
BreezyDR <13521048@std.stei.itb.ac.id>
2023-03-11 11:22:27 +0700

    Create ValueHolder.hpp

---
+++ class/ValueHolder.hpp
@@ -0,0 +1,22 @@
+#ifndef _VALUEHOLDER_HPP_
+#define _VALUEHOLDER_HPP_
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ValueHolder
+{
+private:
+    /* data */
+public:
+    ValueHolder();
+    ~ValueHolder();
+    virtual void value();
+};
+
+
+
+
+
+#endif
