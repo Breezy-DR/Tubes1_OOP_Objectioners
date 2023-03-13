3417236fe440e0ccc8f3241626c645c720b3cab6
BreezyDR <13521048@std.stei.itb.ac.id>
2023-03-13 22:14:34 +0700

    Update AbilityCard.hpp

--- class/AbilityCard.hpp
+++ class/AbilityCard.hpp
@@ -6,13 +6,33 @@
 #define TUBES1_OOP_OBJECTIONERS_ABILITYCARD_HPP
 #include "Abilities/Ability.hpp"
 #include "Card.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
 
 class AbilityCard:public Card,Ability{
 private:
     Ability *ability;
     bool isUsed;
 public:
-    void useCard(){ this->ability->useAbility();}
+    AbilityCard() : Card(), Ability() {
+        this->isUsed = false;
+    }
+    AbilityCard(int _holder, string _abilityname, string _abilitydescription) :
+        Card(_holder), Ability(_abilityname, _abilitydescription) {
+            this->isUsed = false;
+        }
+    ~AbilityCard() {}
+    void useCard(){
+        this->ability->useAbility();
+        this->isUsed = true;
+    }
+    friend ostream &operator<<(ostream &os, AbilityCard& ac) {
+        os << "Holder       : " << ac.getHolder() << endl;
+        os << "Ability name : " << ac.getAbilityName() << endl;
+        os << "Description  : " << ac.getDescription() << endl;
+        return os;
+    }
 };
 
 #endif //TUBES1_OOP_OBJECTIONERS_ABILITYCARD_HPP
