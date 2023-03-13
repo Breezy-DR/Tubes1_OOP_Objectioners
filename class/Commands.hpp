0d9f04cc3c5bbbbddf7ada730ca8b849b4623174
haidarhamda <haidarhamda23@gmail.com>
2023-03-13 22:06:59 +0700

    gtau

---
+++ class/Commands.hpp
@@ -0,0 +1,23 @@
+//
+// Created by suisei on 3/13/23.
+//
+
+#ifndef TUBES1_OOP_OBJECTIONERS_COMMANDS_HPP
+#define TUBES1_OOP_OBJECTIONERS_COMMANDS_HPP
+#include "Player.hpp"
+class Command{
+public:
+    Command(){}
+    void NEXT(){}
+    void REROLL(){}
+    void DOUBLE(){}
+    void QUADRUPLE(){}
+    void HALF(){}
+    void QUARTER(){}
+    void REVERSE(){}
+    void SWAPCARD(){}
+    void SWITCH(){}
+    void ABILITYLESS(){}
+};
+
+#endif //TUBES1_OOP_OBJECTIONERS_COMMANDS_HPP
--- class/Player.hpp
+++ class/Player.hpp
@@ -6,6 +6,7 @@
 #define TUBES1_OOP_OBJECTIONERS_PLAYER_HPP
 #include "PlayerCard.hpp"
 #include <string>
+#include <iostream>
 using namespace std;
 
 
@@ -14,12 +15,23 @@ private:
     int ID;
     string playerName;
     PlayerCard playerCard;
+    bool isCanUseAbility;
 public:
     Player(int id,string playerName){ this->ID=id;
-        this->playerName=playerName;}
+        this->playerName=playerName;
+        this->isCanUseAbility= false;}
     void setPlayerCard(PlayerCard playerCard){ this->playerCard=playerCard;}
+    void setCanUseAbility(bool isCanUse){ this->isCanUseAbility=isCanUse;}
     int getPlayerId(){return this->ID;}
     string getPlayerName(){return this->playerName;}
+    bool getIsCanUse(){return this->isCanUseAbility;};
+    void showPlayerStatus(){
+        cout<<"ID: "<< this->ID<<"\nName: "<< this->playerName<<"\nNormal card:\n";
+
+        for (int i = 0; i < this->playerCard.getMainPlayerCard().size(); ++i) {
+            cout<<""
+        }
+    }
 
 };
 
