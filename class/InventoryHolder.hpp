fed9e0fc963db62462f834ff04278bf8ce6086c5
fazelginanda <fzginanda@gmail.com>
2023-03-13 21:22:05 +0700

    Add PLayerCard and TableCard

--- class/InventoryHolder.hpp
+++ class/InventoryHolder.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include "Card.hpp"
 using namespace std;
 
 class InventoryHolder
---
+++ class/PlayerCard.hpp
@@ -0,0 +1,21 @@
+#ifndef PLAYERCARD_HPP
+#define PLAYERCARD_HPP
+
+#include "InventoryHolder.hpp"
+#include <vector>
+
+class PlayerCard
+{
+private:
+    vector<Card *> MainPlayerCard;
+    Card *AbilityCard;
+
+public:
+    PlayerCard();
+    PlayerCard(const PlayerCard &);
+    ~PlayerCard();
+    vector<Card *> getMainPlayerCard();
+    void showPlayerCard();
+};
+
+#endif
